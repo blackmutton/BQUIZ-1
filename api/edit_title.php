f80bbe7a3d2f31911487a8c20ec2eccf10300c88
yt Chen <yutongc30@gmail.com>
2024-06-13 12:49:59 +0800

    完成標題管理的編輯與刪除功能

---
+++ api/edit_title.php
@@ -0,0 +1,22 @@
+<?php
+$dsn = "mysql:host=localhost;charset=utf8;dbname=dbno1";
+$pdo = new PDO($dsn, 'root', '');
+// print_r($_POST['id']);
+
+foreach ($_POST['id'] as $key => $id) {
+    // print_r($key);
+    // print_r($id);
+    if (!empty($_POST['del'] && in_array($id, $_POST['del']))) {
+        $sql = "delete from `title` where id='$id'";
+        $pdo->exec($sql);
+    } else {
+        if (isset($_POST['show']) && $_POST['show'] == $id) {
+            $sql = "update `title` set `txt` ='{$_POST['txt'][$key]}',`show`='1' where id='$id'";
+        } else {
+            $sql = "update `title` set `txt` ='{$_POST['txt'][$key]}',`show`='0' where id='$id'";
+        }
+        // echo $sql;
+        $pdo->exec($sql);
+    }
+}
+header("location:../admin.php?do=title");
