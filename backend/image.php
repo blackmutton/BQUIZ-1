6c8b65fa052c25c89b897d6e60a55e279c5fc0a0
yt Chen <yutongc30@gmail.com>
2024-06-19 09:22:49 +0800

    完成校園映像檔分頁功能

--- backend/image.php
+++ backend/image.php
@@ -14,7 +14,12 @@
 				// $sql = "select * from title";
 				// $rows = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 				// $rows=q("select * from title");
-				$rows = ${ucfirst($do)}->all();
+				$total = ${ucfirst($do)}->count();
+				$div = 3;
+				$pages = ceil($total / $div);
+				$now = $_GET['p'] ?? 1;
+				$start = ($now - 1) * $div;
+				$rows = ${ucfirst($do)}->all(" limit $start, $div");
 				// print_r($rows);
 				foreach ($rows as $row) {
 				?>
@@ -39,6 +44,30 @@
 				?>
 			</tbody>
 		</table>
+		<div class='cent'>
+			<?php
+			if ($now - 1 >= 1) {
+				$prev = $now - 1;
+				echo "<a href='?do=image&p=$prev'> ";
+				echo "&lt;";
+				// echo "<";
+				echo " </a>";
+			}
+			for ($i = 1; $i <= $pages; $i++) {
+				$size = ($i == $now) ? "24px" : "18px";
+				echo "<a href='?do=image&p=$i'style='font-size:$size;'> ";
+				echo $i;
+				echo " </a>";
+			}
+			if ($now + 1 <= $pages) {
+				$next = $now + 1;
+				echo "<a href='?do=image&p=$next'> ";
+				echo "&gt;";
+				// echo ">";
+				echo " </a>";
+			}
+			?>
+		</div>
 		<table style="margin-top:40px; width:70%;">
 			<tbody>
 				<tr>
