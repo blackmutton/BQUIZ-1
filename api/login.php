8bd417de86fb509f960562943a8bcc7d9d8fb61d
yt Chen <yutongc30@gmail.com>
2024-06-20 13:37:22 +0800

    完成管理者登入功能

--- api/login.php
+++ api/login.php
@@ -1,7 +1,10 @@
 <?php
 session_start();
 include_once "base.php";
-if ($_POST['acc'] == 'admin' && $_POST['pw'] == '1234') {
+// if ($_POST['acc'] == 'admin' && $_POST['pw'] == '1234') {
+$chk=$Admin->count(['acc'=>$_POST['acc'],'pw'=>$_POST['pw']]);
+
+if($chk){
     $_SESSION['login'] = 1;
     to("../admin.php");
     exit();
